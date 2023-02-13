b83d47bc38849fc7b0ea67138ded2823017d8a03
ManarEltokhey <95526721+ManarEltokhey@users.noreply.github.com>
2023-02-13 22:21:28 +0200

    new

---
+++ App.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import { Platform, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+import { Keyboard } from "react-native";
+import CheckBox from "@react-native-community/checkbox";
+
+export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, settaskItems] = useState([]);
+  
+  const handleAddTask = () => {
+    
+    settaskItems([...taskItems, task])
+    setTask(null);
+   Keyboard.dismiss();
+  }
+  const completeTask = (index) => {
+    // Keyboard.dismiss();
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    settaskItems(itemsCopy);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.taskawrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={()=>completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+
+            })
+          }
+          {/* <Task text={'Task 1'}/>
+<Task text={'Task 2'}/> */}
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+
+  },
+  taskawrapper: {
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {
+
+  },
+
+});
